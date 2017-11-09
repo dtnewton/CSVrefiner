5ee2721058d1dccc3448ad31f57610055adb9bfc
dtnewton <dnewton7@uncc.edu>
2017-11-09 17:59:00 -0500

    update

--- src/CSVrefiner.java
+++ src/CSVrefiner.java
@@ -12,20 +12,22 @@ import java.util.Map;
 public class CSVrefiner
 {
 	public static final int NUM_COLS = 12;//this may change
-	
+
 	public static void main(String[] args)
-	{			
+	{
 		ArrayList<String> genres = CSVrefiner.buildGenres();
 		Map<String, Integer> gMap = CSVrefiner.buildGenreMap(genres);
 		System.out.print(gMap.toString());
 		//TODO - build cast/crew list?
-		CSVrefiner.buildCSV(gMap);	          	      
-	}	
-	private static ArrayList<String> buildGenres() 
+		CSVrefiner.buildCSV(gMap);
+	}
+
+	private static ArrayList<String> buildGenres()
 	{
 		ArrayList<String> genres = new ArrayList<String>();
+
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("genre.txt"), "UTF-8")))
-		{		
+		{
 			String ln;
 			while((ln = br.readLine()) != null)
 			{
@@ -34,7 +36,7 @@ public class CSVrefiner
 					ln = ln.substring(1);
 				}
 				genres.add(ln);
-			}			
+			}
 			Collections.sort(genres);
 		}
 		catch(Exception e)
@@ -43,77 +45,82 @@ public class CSVrefiner
 		}
 		return genres;
 	}
+
 	private static Map<String, Integer> buildGenreMap(ArrayList<String> genres)
 	{
-		
+
 		int numVal = 0;
 		Map<String, Integer> gmap = new HashMap<String, Integer>();
 		for(String s : genres)
-		{	
+		{
 			gmap.put(s,numVal);
 			numVal++;
 		}
-		
+
 		return gmap;
 	}
-	private static void buildCSV(Map<String, Integer> gMap) 
+
+	private static void buildCSV(Map<String, Integer> gMap)
 	{
 		ArrayList<String> vals = new ArrayList<String>();
 		String line = null;
-			
+
 		try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("movie_profiles_small.csv"), "UTF-8")))
 		{
 			File file = null;
-			try {
+
+			try
+			{
 				file = new File("movie_profiles_small_updated.csv");
 				if(!file.exists())
 				{
 					file.createNewFile();
 				}
-			}catch(Exception e) {
-				e.printStackTrace();
-				
 			}
-				
-			BufferedWriter writer = new BufferedWriter(new FileWriter(file)); 
-			
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
 			/*first line(column labels)*/
 			line = reader.readLine();
 			if(line.startsWith("\uFEFF"))
 			{
 				line = line.substring(1);
 			}
-			
+
 			String[] splitStr = line.trim().split(",");
-			
+
 			for(int i = 0; i < splitStr.length; i++)
 			{
-			
+
 				vals.add(splitStr[i]);
 			}
-			
+
 			StringBuffer sb = new StringBuffer();
 			for(int i = 0; i < vals.size(); i++)
 			{
 				if(vals.get(i).equals("genres"))
-				{		
+				{
 						sb.append("genre_val");
-						sb.append(",");	
+						sb.append(",");
 				}
 				else
 				{
 					sb.append(vals.get(i));
 					if(i < vals.size() - 1) sb.append(",");
 				}
-				
+
 			}
-					
+
 			line = sb.toString();
 			writer.write(line);
 			writer.newLine();
 			vals.clear();
 			String[] fullLine;
-			
+
 			//*********************************************************************
 			while((line = reader.readLine())!=null)
 			{
@@ -138,7 +145,7 @@ public class CSVrefiner
 						}
 					}
 					else
-					{	
+					{
 						int numericVal = 0;
 						if(vals.get(i).equals(""))
 						{
@@ -151,7 +158,7 @@ public class CSVrefiner
 							numericVal = 0;
 							String[] temp = vals.get(i).split("\\|");
 							//System.out.println(vals.get(i));
-							
+
 							ArrayList<String> innerList = new ArrayList<String>();
 							for(int j = 0; j < temp.length; j++)
 							{
@@ -159,10 +166,10 @@ public class CSVrefiner
 								numericVal += gMap.get(temp[j]);
 
 							}
-												
+
 							writer.write(Integer.toString(numericVal));
 							writer.write(",");
-							
+
 						}
 						else
 						{
@@ -184,6 +191,6 @@ public class CSVrefiner
 		{
 			e.printStackTrace();
 		}
-		//System.out.println(line);	
+		//System.out.println(line);
 	}
 }
