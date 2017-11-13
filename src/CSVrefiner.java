bf6b0b288d872af825a63fb311c609e942309939
dtnewton <dnewton7@uncc.edu>
2017-11-13 16:55:14 -0500

    code cleanup

--- src/CSVrefiner.java
+++ src/CSVrefiner.java
@@ -10,29 +10,18 @@ import java.util.Collections;
 public class CSVrefiner
 {
 	public static final int NUM_COLS = 12;//this may change
-
+	
 	public static void main(String[] args)
-	{
+	{			
 		ArrayList<String> genres = CSVrefiner.buildGenres();
-<<<<<<< HEAD
 		CSVrefiner.buildCSV(genres);	          	      
 	}	
 	
 	private static ArrayList<String> buildGenres() 
-=======
-		Map<String, Integer> gMap = CSVrefiner.buildGenreMap(genres);
-		System.out.print(gMap.toString());
-		//TODO - build cast/crew list?
-		CSVrefiner.buildCSV(gMap);
-	}
-
-	private static ArrayList<String> buildGenres()
->>>>>>> 5ee2721058d1dccc3448ad31f57610055adb9bfc
 	{
 		ArrayList<String> genres = new ArrayList<String>();
-
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("genre.txt"), "UTF-8")))
-		{
+		{		
 			String ln;
 			while((ln = br.readLine()) != null)
 			{
@@ -41,7 +30,7 @@ public class CSVrefiner
 					ln = ln.substring(1);
 				}
 				genres.add(ln);
-			}
+			}			
 			Collections.sort(genres);
 		}
 		catch(Exception e)
@@ -51,68 +40,39 @@ public class CSVrefiner
 		return genres;
 	}
 
-<<<<<<< HEAD
 	private static void buildCSV(ArrayList<String> genres) 
-=======
-	private static Map<String, Integer> buildGenreMap(ArrayList<String> genres)
-	{
-
-		int numVal = 0;
-		Map<String, Integer> gmap = new HashMap<String, Integer>();
-		for(String s : genres)
-		{
-			gmap.put(s,numVal);
-			numVal++;
-		}
-
-		return gmap;
-	}
-
-	private static void buildCSV(Map<String, Integer> gMap)
->>>>>>> 5ee2721058d1dccc3448ad31f57610055adb9bfc
 	{
 		ArrayList<String> headers = new ArrayList<String>();
 		ArrayList<String> vals = new ArrayList<String>();
 		String line = null;
-
+			
 		try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("movie_profiles_small.csv"), "UTF-8")))
 		{
 			File file = null;
-
-			try
-			{
+			try {
 				file = new File("movie_profiles_small_updated.csv");
 				if(!file.exists())
 				{
 					file.createNewFile();
 				}
-			}
-			catch(Exception e)
-			{
+			}catch(Exception e) {
 				e.printStackTrace();
+				
 			}
-<<<<<<< HEAD
 				
 			BufferedWriter writer = new BufferedWriter(new FileWriter(file)); 
 			
 			/*first line - column labels***********************************/
-=======
-
-			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-
-			/*first line(column labels)*/
->>>>>>> 5ee2721058d1dccc3448ad31f57610055adb9bfc
 			line = reader.readLine();
 			if(line.startsWith("\uFEFF"))
 			{
 				line = line.substring(1);
 			}
-
+			
 			String[] splitStr = line.trim().split(",");
-
+			
 			for(int i = 0; i < splitStr.length; i++)
 			{
-<<<<<<< HEAD
 			
 				if(splitStr[i].equals("genres"))
 				{
@@ -120,53 +80,28 @@ public class CSVrefiner
 					{
 						headers.add(genres.get(j));
 					}
-=======
-
-				vals.add(splitStr[i]);
-			}
-
-			StringBuffer sb = new StringBuffer();
-			for(int i = 0; i < vals.size(); i++)
-			{
-				if(vals.get(i).equals("genres"))
-				{
-						sb.append("genre_val");
-						sb.append(",");
->>>>>>> 5ee2721058d1dccc3448ad31f57610055adb9bfc
 				}
 				else
 				{
 					headers.add(splitStr[i]);
 				}
-<<<<<<< HEAD
 			}
 			
 			StringBuffer sb = new StringBuffer();
 			for(int i = 0; i < headers.size(); i++)
 			{
-
 				sb.append(headers.get(i));
-				if(i < headers.size() - 1) sb.append(",");
-			
-=======
-
->>>>>>> 5ee2721058d1dccc3448ad31f57610055adb9bfc
+				if(i < headers.size() - 1) sb.append(",");		
 			}
-
+					
 			line = sb.toString();
 			writer.write(line);
 			writer.newLine();
-<<<<<<< HEAD
 			
 			
 			String[] fullLine, gList;
 			ArrayList<String> gs = new ArrayList<String>();
 			
-=======
-			vals.clear();
-			String[] fullLine;
-
->>>>>>> 5ee2721058d1dccc3448ad31f57610055adb9bfc
 			//*********************************************************************
 			while((line = reader.readLine())!=null)
 			{
@@ -188,30 +123,7 @@ public class CSVrefiner
 						
 						for(int j = 0; j < genres.size(); j++)
 						{
-<<<<<<< HEAD
 							if(gs.contains(genres.get(j)))
-=======
-							writer.write(vals.get(i));
-						}
-					}
-					else
-					{
-						int numericVal = 0;
-						if(vals.get(i).equals(""))
-						{
-							numericVal = 0;
-							writer.write(Integer.toString(numericVal));
-							writer.write(",");
-						}
-						else if(vals.get(i).contains("|"))
-						{
-							numericVal = 0;
-							String[] temp = vals.get(i).split("\\|");
-							//System.out.println(vals.get(i));
-
-							ArrayList<String> innerList = new ArrayList<String>();
-							for(int j = 0; j < temp.length; j++)
->>>>>>> 5ee2721058d1dccc3448ad31f57610055adb9bfc
 							{
 								vals.add("1");
 							}
@@ -219,20 +131,6 @@ public class CSVrefiner
 							{
 								vals.add("0");
 							}
-<<<<<<< HEAD
-=======
-
-							writer.write(Integer.toString(numericVal));
-							writer.write(",");
-
-						}
-						else
-						{
-							numericVal = 0;
-							numericVal = gMap.get(vals.get(i));
-							writer.write(Integer.toString(numericVal));
-							writer.write(",");
->>>>>>> 5ee2721058d1dccc3448ad31f57610055adb9bfc
 						}
 					}
 					else
@@ -268,9 +166,5 @@ public class CSVrefiner
 		{
 			e.printStackTrace();
 		}
-<<<<<<< HEAD
-=======
-		//System.out.println(line);
->>>>>>> 5ee2721058d1dccc3448ad31f57610055adb9bfc
 	}
 }
